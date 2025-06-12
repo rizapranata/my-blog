9e6dacf17737f4b8ca3627150c63aa0bf8038792
Riza <corodotc@gmail.com>
2025-06-12 21:37:48 +0700

    about page styles

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // images: {
-  //   domains: ["example.com"]
-  // }
+  images: {
+    domains: ["images.pexels.com"]
+  }
 };
 
 export default nextConfig;
